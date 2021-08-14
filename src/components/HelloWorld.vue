807d0f328dd502eea0d21ab2ac0442d63409d8aa
miles <a09112332@gmail.com>
2021-08-14 16:45:40 +0800

    style(app): edit some text

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,13 +95,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
-
-
-
-
-
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'HelloWorld',
